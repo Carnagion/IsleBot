e4b605bd7f30d27a59c65e65a8887719b71a4b76
Mclnoot_ <66262586+FanMclaine@users.noreply.github.com>
2021-11-09 09:19:15 +0800

    Create timecheck.js
    Checks time in isleward. Help me carnagion, javascript is ass

---
+++ commands/timecheck.js
@@ -0,0 +1,30 @@
+//probably needs fixing since i dont think it will tell the time in chat
+//halp me carnagion
+
+//Slash Command builder
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+//Slash Command
+const data = new SlashCommandBuilder();
+data.setName("timecheck");
+data.setDescription("Shows what is the current time in isleward");
+
+//the time
+
+//const execute = async function(interaction)
+//{
+//  const d = new Date(timestamp);
+//  date = d.getHours() + ":" + d.getMinutes();
+//  console.log(date);
+//  await interaction.reply({
+//         content: `Time is ${halp me carnagion}`,
+//         ephemeral: true,
+//    });
+//}
+   
+//execute
+module.exports =
+{
+    data,
+    execute,
+}
