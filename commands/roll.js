4ea9fc7aee8d5b51d93ca2c6a58d1144b85ce393
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-11-09 11:58:16 +0000

    Removed redundant comments

--- commands/roll.js
+++ commands/roll.js
@@ -1,12 +1,9 @@
-//Slash command builder
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
-//Slash command
 const data = new SlashCommandBuilder();
 data.setName("roll");
 data.setDescription("Roll a random number between 0-100, both inclusive.");
 
-//Number generator ( Generates number from 0 - 100 )
 const execute = async function(interaction)
 {
     let random = Math.round(Math.random() * 100);
@@ -16,7 +13,6 @@ const execute = async function(interaction)
     });
 }
 
-//execution
 module.exports =
 {
     data,
