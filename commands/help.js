434d39b7a74a4e2fa54fc2c4d1a93656a4fddcee
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-11-15 20:00:23 +0000

    Updated the help command

--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,21 @@
+// credit to Mclnoot
+
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 const data = new SlashCommandBuilder();
 data.setName("help");
-data.setDescription("Shows available commands!");
+data.setDescription("Get information about available commands.");
 
 const execute = async function(interaction)
 {
   await interaction.reply({
-        content: `All available commands:\n/help - Shows available commands!\n/roll - Rolls a random number between 0-100, both inclusive.\n/timecheck - Shows the current time in isleward.\nThis bot is not complete, feel free to contribute by creating a pull request on https://github.com/Carnagion/IsleBot :D`,
+        content: "All available commands:\
+        \n`/help` - Get information about available commands.\
+        \n`/roll` - Rolls a random number between 0-100, both inclusive.\
+        \n`/time` - Shows the current time in isleward.\
+        \n`/fishrod` - Find out where to obtain a fishing rod.\
+        \n`/herbs` - Find out how to find and harvest herbs.\
+        \nThis bot is not complete. Feel free to contribute by creating a pull request on https://github.com/Carnagion/IsleBot. :D",
   });
 }  
 
