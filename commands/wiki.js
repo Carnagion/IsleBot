d3d7b6a23aed15f68ea3d37b6753d13f98e73bab
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-11-16 10:36:24 +0000

    Updated wiki command

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,13 +1,17 @@
+// credit to Mclnoot
+
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 const data = new SlashCommandBuilder();
 data.setName("wiki");
-data.setDescription("Gives information about the iwd wiki");
+data.setDescription("Find out more about the IsleWard wiki.");
 
 const execute = async function(interaction)
 {
     await interaction.reply({
-        content: "Need help in isleward? visit the wiki (https://wiki.isleward.com/Main_Page) for informations and tips! :D. \nThe wiki has some guides for **Begginers** and **Advanced** players too.",
+        content: "Need help in IsleWard? Visit the **wiki** at https://wiki.isleward.com/Main_Page for more information and tips!\
+        \nThe wiki has some guides for both beginners and and advanced players.\
+        \n(Psst: the wiki is community-maintained. If you see information that may look outdated or wrong, feel free to create an account and start helping out the community by editing the wiki. Every little bit helps.)",
     });
 }
 
