6b4b250c5f18ddc484a065a3d9faea6237870185
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-11-16 21:16:09 +0000

    Added herb phrase

---
+++ phrases/herb.js
@@ -0,0 +1,27 @@
+const { similarity } = require("../utility/helper/similarity.js");
+
+const keywords = [
+    ["herb", "herbs"],
+    ["gather", "harvest", "find", "get"],
+    ["how", "where"],
+];
+
+const match = function(words)
+{
+    return similarity(words, keywords) >= 2 / 3;
+}
+
+const respond = function(message)
+{
+    message.reply({
+        content: "Herbs can be found scattered on the ground all around **Fjolarok** and **Fjolgard**. They can be picked up by standing over them and pressing either `U` or `G`.\
+        \nPressing `V` to toggle nameplates can help find them more easily if you aren't used to what they look like.",
+    });
+}
+
+module.exports =
+{
+    keywords,
+    match,
+    respond,
+}
