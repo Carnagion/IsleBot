f9f3fdc72fc185b0f44bd1ec1118dfa16670345c
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-11-17 10:07:44 +0000

    Updated commands to use constant defined values

--- commands/fishrod.js
+++ commands/fishrod.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { fishingrod } = require("../constants.json");
 
 const data = new SlashCommandBuilder();
 data.setName("fishrod");
@@ -7,8 +8,7 @@ data.setDescription("Find out where to obtain a fishing rod.");
 const execute = async function(interaction)
 {
     await interaction.reply({
-        content: "A competition fishing rod can be obtained for free from **Angler Nayla**. She appears to the right of the bridge that leads to Fjolgard, just above Thumper's cave.\
-        \nAlternatively, a fishing rod can be bought from the **Hermit**.",
+        content: fishingrod,
     });
 }
 
--- commands/herbs.js
+++ commands/herbs.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { herbs } = require("../constants.json");
 
 const data = new SlashCommandBuilder();
 data.setName("herbs");
@@ -7,8 +8,7 @@ data.setDescription("Find out how to find and harvest herbs.");
 const execute = async function(interaction)
 {
     await interaction.reply({
-        content: "Herbs can be found scattered on the ground all around **Fjolarok** and **Fjolgard**. They can be picked up by standing over them and pressing either `U` or `G`.\
-        \nPressing `V` to toggle nameplates can help find them more easily if you aren't used to what they look like.",
+        content: herbs,
     });
 }
 
