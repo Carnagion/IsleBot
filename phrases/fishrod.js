a361eb174165155ea763f20f45d54958676a8bf8
Mclnoot_ <66262586+FanMclaine@users.noreply.github.com>
2021-11-17 17:40:56 +0800

    Added fishrod phrase (#10)

---
+++ phrases/fishrod.js
@@ -0,0 +1,27 @@
+const { similarity } = require("../utility/helper/similarity.js");
+
+const keywords = [
+    ["fishing", "fish", "fishing rod", "sun carp", "fishrod",],
+    ["find", "get", "gather"],
+    ["how", "where"],
+];
+
+const match = function(words)
+{
+    return similarity(words, keywords) >= 2 / 3;
+}
+
+const respond = function(message)
+{
+    message.reply({
+        content: "A competition fishing rod can be obtained for free from **Angler Nayla**. She appears to the right of the bridge that leads to Fjolgard, just above Thumper's cave.\
+        \nAlternatively, a fishing rod can be bought from the **Hermit**.",
+    });
+}
+
+module.exports =
+{
+    keywords,
+    match,
+    respond,
+}
