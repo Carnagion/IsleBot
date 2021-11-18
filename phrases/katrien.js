1ac62d153dcdb3fe2b2e4824dcebd571e411ab20
Mclnoot_ <66262586+FanMclaine@users.noreply.github.com>
2021-11-18 18:38:38 +0800

    good

---
+++ phrases/katrien.js
@@ -0,0 +1,26 @@
+const { similarity } = require("../utility/helper/similarity.js");
+const { katrien } = require("../constants.json");
+
+const keywords = [
+    ["katrien"],
+    ["lore"],
+];
+
+const match = function(words)
+{
+    return similarity(words, keywords) >= 2 / 3;
+}
+
+const respond = function(message)
+{
+    message.reply({
+        content: "Katrien lore? hmmm I agree, that bitch has been acting sus lately." //Note: dont do something like "go over to #fan-lore and discuss it" NO i want to sound it like reddit's automoderator's 'based' phrase
+    });
+}
+
+module.exports =
+{
+    keywords,
+    match,
+    respond,
+}
