0cf375cbb2a2c3fd120424535203f55484a6ae18
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-11-18 15:46:00 +0000

    Updated roles phrase to use defined string

--- phrases/roles.js
+++ phrases/roles.js
@@ -1,4 +1,5 @@
 const { similarity } = require("../utility/helper/similarity.js");
+const { roles } = require("../constants.json");
 
 const keywords = [
     ["roles", "role", "stowaway", "deckhand", "cannoneer", "quartermaster", "captain", "admiral"],
@@ -14,13 +15,7 @@ const match = function(words)
 const respond = function(message)
 {
     message.reply({
-        content: "Here is a brief explanation on the roles in this server:\
-        \n`Stowaway`: Active players\
-        \n`Deckhand`: Even more active and helpful players\
-        \n`Cannoneer`: Players who have donated to IsleWard\
-        \n`Quartermaster`: Players who support IsleWard on Patreon\
-        \n`Captain`: Players who contributed to IsleWard's development\
-        \n`Admiral`: Only the coolest of the cool; i.e. the main developers.",
+        content: roles,
     });
 }
 
