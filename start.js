3472b84b705411acbc7e83d0ec94c5edc341e793
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-11-20 18:27:45 +0000

    Updated entry point code

--- start.js
+++ start.js
@@ -3,9 +3,11 @@ const environment = require("dotenv");
 const { login } = require("./utility/login.js");
 const { upload } = require("./utility/upload.js");
 const { listen } = require("./utility/listen.js");
+const { intercept } = require("./utility/intercept.js");
 
 environment.config();
 
 login();
 upload();
 listen();
+intercept();
