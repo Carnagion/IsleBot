22618b3522a834bc4db6d8fe1293ac8963cfbab4
Mclnoot_ <66262586+FanMclaine@users.noreply.github.com>
2021-11-26 18:38:31 +0800

    china

---
+++ phrases/trade.js
@@ -0,0 +1,28 @@
+//any1 have good wand or neck item to trade? pls (contact: whentheimpos)
+const { similarity } = require("../utility/helper/similarity.js");
+const { herbs } = require("../constants.json");
+
+const keywords = [
+    ["trade", "trading"],
+    ["to"],
+    ["how"],
+];
+
+const match = function(words)
+{
+    return similarity(words, keywords) >= 2 / 3;
+}
+
+const respond = function(message)
+{
+    message.reply({
+        content: tradeandstuffs,
+    });
+}
+
+module.exports =
+{
+    keywords,
+    match,
+    respond,
+}
