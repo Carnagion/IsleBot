d9c499b02be58e7875f6e78cde047fba24902137
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-11-30 10:42:46 +0000

    Added explain command to prevent redundant commands

---
+++ commands/explain.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { bot } = require("../utility/login.js");
+
+const data = new SlashCommandBuilder();
+data.setName("explain");
+data.setDescription("Show a brief explanation of a specified IsleWard mechanic or feature.");
+data.addStringOption(option => option.setName("feature").setDescription("The feature to explain.").setRequired(true));
+
+const execute = async function(interaction)
+{
+    const feature = interaction.options.getString("feature");
+    const phrase = bot.phrases.get(feature);
+    if (phrase == null)
+    {
+        await interaction.reply({
+            content: "That feature either doesn't exist, or does not have an explanation defined for it.",
+            ephemeral: true,
+        });
+    }
+    else
+    {
+        phrase.respond(interaction);
+    }
+}
+
+module.exports =
+{
+    data,
+    execute,
+}
